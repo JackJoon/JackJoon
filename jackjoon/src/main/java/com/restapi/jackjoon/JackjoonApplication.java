d1c004974a7e572200d5d7b34c0369cd8a2cedda
dhdlxor@naver.com <dnjfrmq!29>
2022-03-08 01:41:46 +0900

    Initial commit

---
+++ jackjoon/src/main/java/com/restapi/jackjoon/JackjoonApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.jackjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackjoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackjoonApplication.class, args);
+	}
+
+}
---
+++ jackjoon/src/test/java/com/restapi/jackjoon/JackjoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.jackjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackjoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
